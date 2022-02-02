9da102a4820f3de34417a77ac30b0becd31e49fb
Rodrigo Cavanha <15946186+rdcavanha@users.noreply.github.com>
2022-02-02 01:45:39 -0300

    chore(eslint): simplifies eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,59 @@
 const extensions = ['.ts', '.tsx'];
 
 module.exports = {
-  plugins: ['import'],
-  extends: ['airbnb', 'prettier', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': extensions,
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
+  plugins: ['import', 'react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-console': ['error', { allow: ['error'] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'import/prefer-default-export': 'off',
+    'arrow-body-style': ['error', 'as-needed'],
+    'import/no-unresolved': 'error',
+  },
   overrides: [
     {
-      files: ['*.tsx', '*.ts'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint', 'import'],
-      extends: ['airbnb', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
-      settings: {
-        'import/extensions': extensions,
-        'import/parsers': {
-          '@typescript-eslint/parser': extensions,
-        },
-        'import/resolver': {
-          node: {
-            extensions,
-          },
-          typescript: {},
-        },
-      },
-      rules: {
-        'react/react-in-jsx-scope': 'off',
-        'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
-        'react/prop-types': 'off',
-        'react/jsx-filename-extension': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'import/prefer-default-export': 'off',
-        'arrow-body-style': ['error', 'as-needed'],
-        'no-use-before-define': ['error', { functions: false }],
-        'jsx-a11y/label-has-associated-control': [
-          'error',
-          {
-            required: {
-              some: ['nesting', 'id'],
-            },
-          },
-        ],
-        'jsx-a11y/label-has-for': [
-          'error',
-          {
-            required: {
-              some: ['nesting', 'id'],
-            },
-          },
-        ],
-        'import/extensions': [
-          'error',
-          'ignorePackages',
-          {
-            ts: 'never',
-            tsx: 'never',
-          },
-        ],
-      },
-    },
-    {
-      files: ['*.test.ts', '*.test.tsx', './*.ts'],
+      files: ['*.test.ts', '*.test.tsx', './*.ts', '**/__tests__/*.*'],
       rules: {
         'import/no-extraneous-dependencies': 'off',
-      },
-    },
-    {
-      files: ['*.d.ts'],
-      rules: {
-        '@typescript-eslint/no-empty-interface': 'off',
       },
     },
   ],
